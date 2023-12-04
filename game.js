810251ee0ecc17a490f88fef3e9fcc61772c68fb
Mathews1004 <mathewsosbr@gmail.com>
2023-12-04 09:11:25 -0300

    add responsive questions and answers

--- game.js
+++ game.js
@@ -1,19 +1,20 @@
-const el = document.getElementById('el');
-const choices = Array.from(document.getElementsByClassName('choice-Text'));
+const question = document.getElementById('question');
+const choices = Array.from(document.getElementsByClassName('choice-text'));
+
 
 let currentQuestion = {};
-let acceptinAnswers = true;
+let acceptingAnswers = false;
 let score = 0;
-let questionsCouter = 0;
+let questionCounter = 0;
 let availableQuestions = [];
 
 let questions = [
   {
-    question: "Inside which HTML element do we put the JavaScript",
-    choice1: "<script>",
-    choice2: "<javascript>",
-    choice3: "<js>",
-    choice4: "<scripting>",
+    question: 'Inside which HTML element do we put the JavaScript ?',
+    choice1: '<script>',
+    choice2: '<javascript>',
+    choice3: '<js>',
+    choice4: '<scripting>',
     answer: 1,
   },
   {
@@ -26,17 +27,16 @@ let questions = [
     answer: 3,
   },
   {
-    question:
-      "How do you write 'Hello World' in an alert box ?",
+    question: "How do you write 'Hello World' in an alert box ?",
     choice1: "msgBox('Hello World');",
     choice2: "alertBox('Hello World');",
     choice3: "msg('Hello World');",
     choice4: "alert('Hello World');",
     answer: 4,
   },
-]
+];
 
-//CONSTANTS 
+//CONSTANTS
 const CORRECT_BONUS = 10;
 const MAX_QUESTIONS = 3;
 
@@ -44,13 +44,39 @@ startGame = () => {
   questionCounter = 0;
   score = 0;
   availableQuestions = [...questions];
+  console.log(availableQuestions);
   getNewQuestion();
 };
 
 getNewQuestion = () => {
+  if(availableQuestions.length === 0 || questionCounter >= MAX_QUESTIONS){
+    //go to the end page
+    return window.location.assign("/end.html");
+  }
   questionCounter++;
   const questionIndex = Math.floor(Math.random() * availableQuestions.length);
-    currentQuestion = availableQuestions[questionIndex]
-}   
+  currentQuestion = availableQuestions[questionIndex];
+  question.innerText = currentQuestion.question;
+
+  choices.forEach( choice => {
+    const number = choice.dataset['number']
+    choice.innerText = currentQuestion["choice" + number]
+  });
+
+  availableQuestions.splice(questionIndex, 1);
+  acceptingAnswers = true;
+};      
+
+choices.forEach(choice => {
+  choice.addEventListener("click", e => {
+    if (!acceptingAnswers) return;
+
+    acceptingAnswers = false;
+    const selectedChoice = e.target;
+    const selectedAnswer = selectedChoice.dataset["number"];
+    console.log(selectedAnswer);
+    getNewQuestion();
+  });
+});
 
 startGame();
