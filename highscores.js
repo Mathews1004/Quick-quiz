654696448ef1f209277e80038f80c5d195280e8d
Mathews1004 <mathewsosbr@gmail.com>
2023-12-13 16:48:11 -0300

    get the questions from an open api

--- highscores.js
+++ highscores.js
@@ -1,8 +1,9 @@
-const highScoresList = document.getElementById('highScoresList');
+const highScoresList = document.getElementById("highScoresList");
 const highScores = JSON.parse(localStorage.getItem("highScores")) || [];
 
-console.log(
-    highScores.map(score => {
+highScoresList.innerHTML = highScores
+    .map(score => {
         return `<li class = "high-score">${score.name} - ${score.score}</li>`;
     })
-);
+    .join("");
+
